2bfc9a00238183849ecc36583ec9e59c3105482d
Raffy Anyelo <elraffy3@gmail.com>
2020-11-30 14:34:40 -0400

    onSearch added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) }, { path: 'character-list', loadChildren: () => import('./components/pages/characters/character-list/character-list.module').then(m => m.CharacterListModule) }, { path: 'character-details', loadChildren: () => import('./components/pages/characters/character-details/character-details.module').then(m => m.CharacterDetailsModule) }];
+const routes: Routes = [
+  {path:'', redirectTo:'home', pathMatch:'full'},
+  { path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) },
+  { path: 'character-list', loadChildren: () => import('./components/pages/characters/character-list/character-list.module').then(m => m.CharacterListModule) },
+  { path: 'character-details/:id', loadChildren: () => import('./components/pages/characters/character-details/character-details.module').then(m => m.CharacterDetailsModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/shared/components/form-search/form-search.component.ts
+++ src/app/shared/components/form-search/form-search.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form-search',
-  templateUrl: './form-search.component.html',
-  styleUrls: ['./form-search.component.css']
+  template: `
+  <input
+      #inputSearch
+      autofocus
+      type="text"
+      class="form-control-lg"
+      placeholder="Search..."
+      (keyup)="onSearch(inputSearch.value)">
+  `,
+  styles: [' input {width: 100%;}']
 })
 export class FormSearchComponent implements OnInit {
 
-  constructor() { }
+  constructor( private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onSearch( value: string){
+    if( value && value.length >3 ){
+      this.router.navigate(['/character-list'],{
+        queryParams:  { q: value }
+      });
+
+    }
   }
 
 }
