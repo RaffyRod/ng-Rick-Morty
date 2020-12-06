29cdab53135214a40375b84660b059e43a27f6bd
Raffy Anyelo <elraffy3@gmail.com>
2020-12-06 13:25:04 -0400

    service's methods created

--- src/app/shared/services/character.service.ts
+++ src/app/shared/services/character.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient }  from '@angular/common/http';
+import { Character } from '../interfaces/character.interface';
+import { environment } from '@environment/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CharacterService {
 
-  constructor( ) { }
+  constructor( private http: HttpClient ) { }
 
-  searchCharacters(query = '', page = 1){}
-  getDetails(id:number){}
+  searchCharacters(query = '', page = 1){
+    const filter =  `${environment.baseUrlAPI}/?name=${query}&page=${page}`;
+    return this.http.get<Character[]>( filter );
+  }
+  getDetails(id:number){
+    return this.http.get<Character>(`${environment.baseUrlAPI}/${id}`)
+  }
 }
