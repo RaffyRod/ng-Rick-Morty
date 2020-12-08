196146268d36c5b0be4bc56d4fe4d4db11e7330e
Raffy Anyelo <elraffy3@gmail.com>
2020-12-08 10:50:47 -0400

    character details ready

--- src/app/components/pages/characters/character-details/character-details.component.ts
+++ src/app/components/pages/characters/character-details/character-details.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common'
+
+import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators'
+
+import { Character } from '../../../../shared/interfaces/character.interface';
+import { CharacterService } from '@app/shared/services/character.service';
 
 @Component({
   selector: 'app-character-details',
@@ -6,10 +14,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./character-details.component.css']
 })
 export class CharacterDetailsComponent implements OnInit {
+  character$: Observable<Character>;
 
-  constructor() { }
+  constructor( private route: ActivatedRoute,
+               private characterSvc: CharacterService,
+               private location: Location) { }
 
   ngOnInit(): void {
+    this.route.params.pipe( take(1)).subscribe((params) =>{
+      const id = params['id'];
+      this.character$ = this.characterSvc.getDetails(id);
+    })
   }
+  onGoBack(){
+    this.location.back();
+  }
+
 
 }
